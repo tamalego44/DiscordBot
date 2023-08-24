dfd622dbce080debb7629cca66b7c0ae381999d3
James Purcell <jamespur@vt.edu>
2023-08-23 22:55:48 -0400

    clean up files

--- kill.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ps aux | grep "python bot.py" | awk '{print $2}' | xargs kill -9 &>/dev/null
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ps aux | grep "python bot.py" | awk '{print $2}' | xargs kill -9 &>/dev/null
-
-nohup python bot.py &
-
-
